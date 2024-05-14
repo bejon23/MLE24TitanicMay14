9ee7de2b428569d09cf367ccc6d5f8c84a75abd5
ronysaimon16 <167416399+ronysaimon16@users.noreply.github.com>
2024-05-14 13:48:42 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sklearn.preprocessing import LabelEncoder
 import pickle
 import os
 
 app = FastAPI()
-
-import os
+templates = Jinja2Templates(directory="MLE24Titanic/templates")
 
 # Get the absolute path of the current file
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,10 +17,17 @@ templates_dir = os.path.join(current_dir, "templates")
 # Initialize Jinja2Templates with the correct directory
 templates = Jinja2Templates(directory=templates_dir)
 
-
 # Load the trained model
 with open('stacking_clf.pkl', 'rb') as f:
     stacking_clf = pickle.load(f)
+
+# Define label encoders for categorical features
+label_encoders = {}
+categorical_features = ['sex', 'embarked']
+
+for feature in categorical_features:
+    label_encoders[feature] = LabelEncoder()
+    label_encoders[feature].fit(df[feature])
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
@@ -29,8 +36,12 @@ async def home(request: Request):
 @app.post("/predict", response_class=HTMLResponse)
 async def predict(request: Request, pclass: int = Form(...), sex: str = Form(...), age: int = Form(...), sibsp: int = Form(...),
                    parch: int = Form(...), fare: int = Form(...), embarked: str = Form(...)):
+    # Convert categorical features to numerical format
+    sex_encoded = label_encoders['sex'].transform([sex])[0]
+    embarked_encoded = label_encoders['embarked'].transform([embarked])[0]
+
     # Make prediction
-    prediction = stacking_clf.predict([[pclass, sex, age, sibsp, parch, fare, embarked]])
+    prediction = stacking_clf.predict([[pclass, sex_encoded, age, sibsp, parch, fare, embarked_encoded]])
     result = "likely" if prediction == 1 else "unlikely"
     
     return templates.TemplateResponse("results.html", {"request": request, "prediction": result})
